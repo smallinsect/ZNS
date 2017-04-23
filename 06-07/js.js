e18cf189a10c98018d1ab773f9b286b7ffbbb2bd
loveinsect <513312514@qq.com>
2017-04-23 16:43:35 +0800

    merge 06 07 --> 06-07

--- 06-07/js.js
+++ 06-07/js.js
@@ -29,4 +29,25 @@ window.onload = function () {
 	oBtn.onclick = function () {
 		oTxt1.value = parseInt(oTxt1.value) + parseInt(oTxt2.value);
 	};
+
+	var aUl = document.getElementsByTagName('ul')[0];
+	var aLi = document.getElementsByTagName('li');
+	// 隔行变色
+	for(var i = 0; i < aLi.length; ++i){
+		if(i % 2 == 0){
+			aLi[i].style.background = '#ccc';
+		}else{
+			aLi[i].style.background = '#bbb';
+		}
+	}
+	// 遍历数组
+	// var arr = [1, 2, 3, 4, 5, 6];
+	// for(var i in arr){
+	// 	alert(arr[i]);
+	// }
+	// 遍历json
+	// var json = {1: 'a', 'b': 2, 'Fun': 'd', };
+	// for(var i in json){
+	// 	alert(json[i]);
+	// }
 };
